95c5a2c9f16e417e37a2bf5081c9425734fb6b7b
Mike MacCana <mike.maccana@gmail.com>
2010-10-20 15:48:22 +0100

    - Improve response for failed requests - added additional HTTP error codes, don't try and JSON decode errors. - Documentation improvements
    Signed-off-by: Mike MacCana <mike.maccana@gmail.com>

--- README.markdown
+++ README.markdown
@@ -3,9 +3,11 @@ Python DNSimple
 
 ## Introduction
 
-This is a client for the DNSimple REST API. It allows you to fetch existing domain info, and register new domains. More features are on the way in future.
+This is a client for the DNSimple REST API. It currently allows you to fetch existing domain info, as well as bulk-register new domains. More features are on the way.
 
 ### Getting started
+
+You'll need the simplejson module installed.
 
 Create a file called .dnsimple in the current dir with the following data:
 
@@ -15,21 +17,58 @@ Create a file called .dnsimple in the current dir with the following data:
 Then import the module:
 
 	from dnsimple import DNSimple
-	dns = DNSimple(username,password) 
+
+And create a DNSimple object:	
+	
+	dns = DNSimple() 
 
 ### Check out your existing domains:
 
 Just run:
 
 	domains = dns.getdomains()
-	for domain in domains:
-	    print domain['domain']['name']
+
+Results appear as a Python dict:
+
+	{'domain': {'created_at': '2010-10-14T09:45:32Z',
+	            'expires_at': '10/14/2011 5:45:00 AM',
+	            'id': 999,
+	            'last_enom_order_id': None,
+	            'name': 'yourdomain.com',
+	            'name_server_status': 'active',
+	            'registrant_id': 99,
+	            'registration_status': 'registered',
+	            'updated_at': '2010-10-14T10:00:14Z',
+	            'user_id': 99}},
+	{'domain': {'created_at': '2010-10-15T16:02:34Z',
+	            'expires_at': '10/15/2011 12:02:00 PM',
+	            'id': 999,
+	            'last_enom_order_id': None,
+	            'name': 'anotherdomain.com',
+	            'name_server_status': 'active',
+	            'registrant_id': 99,
+	            'registration_status': 'registered',
+	            'updated_at': '2010-10-15T16:30:16Z',
+	            'user_id': 99}}]
+
+
+### Check out a specific domain
+
+	dns.getdomain('mikemaccana.com')
+
+Results are the same as getdomains() above, but only show the domain specified.
 
 ### Register a new domain
 
 Just run:
 
-	test = dnsimple.register('yourdomain.com','16')
+	dns.register('newdomain.com')
+
+This will register 'newdomain.com', automatically picking the registrant\_id from your first domain. To specify a particularly registrant\_id, just run:
+
+	dns.register('newdomain.com',99)
+
+Responses will be in a dictionary describing the newly created domain, same as the getdomain() above.
 	
 ### License
 
--- dnsimple.py
+++ dnsimple.py
@@ -9,34 +9,55 @@ from urllib2 import Request, urlopen, URLError
 import urllib
 import re
 from BaseHTTPServer import BaseHTTPRequestHandler
-import simplejson as json
+# Update Pythons list of error codes with some that are missing
+newhttpcodes = {
+    422:('Unprocessable Entity','HTTP_UNPROCESSABLE_ENTITY'),
+    423:('Locked','HTTP_LOCKED'),
+    424:('Failed Dependency','HTTP_FAILED_DEPENDENCY'),
+    425:('No code','HTTP_NO_CODE'),
+    426:('Upgrade Required','HTTP_UPGRADE_REQUIRED'),
+}
+for code in newhttpcodes:
+    BaseHTTPRequestHandler.responses[code] = newhttpcodes[code]
 
+import simplejson as json
+import logging
 
 class DNSimple(object):
-    def __init__(self,username,password):
-        self.endpoint = 'https://dnsimple.com'
-        self.authstring = self.getauthstring(self.endpoint,username,password)
-        self.useragent = 'DNSimple Python API v20101015'
+    def __init__(self):
+        try:
+            passwordfile = open('.dnsimple').read()
+            username = re.findall(r'username:.*', passwordfile)[0].split(':')[1].strip()
+            password = re.findall(r'password:.*', passwordfile)[0].split(':')[1].strip()
+        except: 
+            logging.warn('Could not open .dnsimple file - see README.markdown')    
+            exit()        
+        self.__endpoint = 'https://dnsimple.com'
+        self.__authstring = self.__getauthstring(self.__endpoint,username,password)
+        self.__useragent = 'DNSimple Python API v20101015'
 
-    def getauthstring(self,endpoint,username,password):
+    def __getauthstring(self,__endpoint,username,password):
         encodedstring = base64.encodestring(username+':'+password)[:-1]
         return "Basic %s" % encodedstring
 
-    def resthelper(self,url,postdata=None):    
+    def __resthelper(self,url,postdata=None):    
         '''Does GET requests and (if postdata specified) POST requests.
         For POSTs we do NOT encode our data, as DNSimple's REST API expects square brackets
         which are normally encoded according to RFC 1738. urllib.urlencode encodes square brackets 
         which the API doesn't like.'''
-        url = self.endpoint+url
-        request = Request(url, postdata, {"Authorization": self.authstring, "User-Agent": self.useragent })
-        result = self.requesthelper(request)
-        return json.loads(result)        
+        url = self.__endpoint+url
+        request = Request(url, postdata, {"Authorization": self.__authstring, "User-Agent": self.__useragent })
+        result = self.__requesthelper(request)
+        if result:
+            return json.loads(result)        
+        else:
+            return None
 
-    def deletehelper(self,url):    
+    def __deletehelper(self,url):    
         '''Does DELETE requests.'''
         raise Exception('Not implemented yet')
                   
-    def requesthelper(self,request):
+    def __requesthelper(self,request):
         '''Does requests and maps HTTP responses into delicious Python juice'''
         try:
             handle = urlopen(request)            
@@ -45,50 +66,49 @@ class DNSimple(object):
             if hasattr(e, 'reason'):
                 print 'We failed to reach a server.'
                 print 'Reason: ', e.reason
+                return
             elif hasattr(e, 'code'):
                 print 'Error code: ', e.code
                 print '\n'.join(BaseHTTPRequestHandler.responses[e.code])
+                return
         else:
             return handle.read()    
         
     def getdomains(self):
         '''Get a list of all domains in your account.'''
-        return self.resthelper('/domains.json')
+        return self.__resthelper('/domains.json')
 
     def getdomain(self,domain):
         '''Get the details for a specific domain in your account. .'''
-        return self.resthelper('/domains/'+domain+'.json')
+        return self.__resthelper('/domains/'+domain+'.json')
 
     def getdomain(self,domain):
         '''Get the details for a specific domain in your account. .'''
-        return self.resthelper('/domains/'+domain+'.json')
+        return self.__resthelper('/domains/'+domain+'.json')
 
-    def register(self,domainname,registrant_id):
+    def register(self,domainname,registrant_id=None):
         '''Register a domain name with DNSimple and the appropriate domain registry. '''
+        if not registrant_id:
+            # Get the registrant ID from the existing domains
+            try:
+                registrant_id = str(self.getdomains()[0]['domain']['registrant_id'])
+            except:
+                print 'Could not find registrant_id! Please specify manually.'
+                exit
         postdata = 'domain[name]='+domainname+'&domain[registrant_id]='+registrant_id
-        return self.resthelper('/domain_registrations.json', postdata)
+        return self.__resthelper('/domain_registrations.json', postdata)
 
     def transfer(self,domainname,registrant_id):
         '''Transfer a domain name from another domain registrar into DNSimple. '''
         postdata = 'domain[name]='+domainname+'&domain[registrant_id]='+registrant_id
-        return self.resthelper('/domain_transfers.json', postdata)    
+        return self.__resthelper('/domain_transfers.json', postdata)    
 
     def adddomains(self, domainname):
         '''Create a single domain in DNSimple in your account.'''
         postdata = 'domain[name]='+domainname
-        return self.resthelper('/domains.json', postdata)          
+        return self.__resthelper('/domains.json', postdata)          
 
     def delete(self,domain):
         '''Delete the given domain from your account. You may use either the domain ID or the domain name.'''
-        return self.deletehelper('/domains/'+domain+'.json')
+        return self.__deletehelper('/domains/'+domain+'.json')
                
-
-def main():
-    passwordfile = open('.dnsimple').read()
-    username = re.findall(r'username:.*', passwordfile)[0].split(':')[1].strip()
-    password = re.findall(r'password:.*', passwordfile)[0].split(':')[1].strip()
-    dnsimple = DNSimple(username,password) 
-    print 'You should add some code here - check out the README'    
-        
-if __name__ == '__main__':
-    main()
