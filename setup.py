3fcc36a683d4089092d9eccf6fa186580fe545c5
Alex Coomans <alex@alexcoomans.com>
2013-12-09 21:03:18 -0600

    A few changes that enabled pip releases

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import setup, find_packages
 
 from dnsimple import dnsimple
 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.markdown'), 'r') as readme_file:
+    readme = readme_file.read()
+
 setup(
-    name='dnsimple-python',
+    name='dnsimple',
     version=dnsimple.__version__,
     description=u' '.join(dnsimple.__doc__.splitlines()).strip(),
-    long_description=os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), 'README.markdown'),
+    long_description=readme,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    url='https://github.com/ixc/dnsimple-python/',
+    url='https://github.com/mikemaccana/dnsimple-python/',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
