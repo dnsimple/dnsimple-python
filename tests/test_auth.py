01380079aa4a85d99dbe2165f819893c2972141d
Chris Morgan <chris.j.r.morgan@gmail.com>
2014-09-27 05:08:45 +1200

    hotfix for sending unencoded auth details over network/auth failure

---
+++ tests/test_auth.py
@@ -0,0 +1,23 @@
+__author__ = 'Chris Morgan'
+
+import unittest
+from dnsimple import DNSimple, DNSimpleException
+
+
+class AuthTestCase(unittest.TestCase):
+
+    def test_username_and_password_auth(self):
+        dns = DNSimple(username='chris.morgan@youngshand.com', password='dnsimple-python', sandbox=True)
+        self.assertTrue(type(dns.domains()) is list)
+
+    def test_email_and_api_token_auth(self):
+        dns = DNSimple(email='chris.morgan@youngshand.com', api_token='L0lkPizHnqVrsIyViLpB', sandbox=True)
+        self.assertTrue(type(dns.domains()) is list)
+
+    def test_config_auth(self):
+        dns = DNSimple(sandbox=True)
+        self.assertTrue(type(dns.domains()) is list)
+
+
+if __name__ == '__main__':
+    unittest.main()
