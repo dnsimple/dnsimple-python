98f10bd654ca699a3a9018df63ab41b44f0412f4
Ivan Bakalov <ivan.bakalov@dnsimple.com>
2022-08-17 10:56:32 +0300

    Exceptions print their error message (#340)
    * Exception class initializes super and overrides __str__ method
    
    * Use : instead of ->

--- dnsimple/exceptions.py
+++ dnsimple/exceptions.py
@@ -1,4 +1,19 @@
 class DNSimpleException(Exception):
+    """
+    Root Exception raised for errors in the client.
+
+    :param message: str
+        The raw response from the server
+    :param errors: array[str]
+        An array of error messages
+    """
+
     def __init__(self, message=None, errors=None):
         self.message = message
         self.errors = errors
+        super().__init__(self.message)
+
+    def __str__(self):
+        if self.errors is None:
+            return self.message
+        return f'{self.message}: {"".join(self.errors, ",")}'
