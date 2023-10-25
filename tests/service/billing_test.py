c3842ee72276ca63c9015e86dd7c064840189531
Wilson Lin <wilson.lin@dnsimple.com>
2023-10-26 10:05:21 +1100

    Add tests

---
+++ tests/service/billing_test.py
@@ -0,0 +1,44 @@
+import unittest
+
+import responses
+
+from dnsimple import DNSimpleException
+from dnsimple.struct import Charge
+from tests.helpers import DNSimpleMockResponse, DNSimpleTest
+
+
+class BillingTest(DNSimpleTest):
+    @responses.activate
+    def test_list_charges_success(self):
+        responses.add(DNSimpleMockResponse(method=responses.GET,
+                                           path='/1010/billing/charges',
+                                           fixture_name='listCharges/success'))
+        charges = self.billing.list_charges(1010).data
+
+        self.assertEqual(charges, [
+            Charge({"invoiced_at": "2023-08-17T05:53:36Z","total_amount": "14.50","balance_amount": "0.00","reference": "1-2","state": "collected","items":[{"description": "Register bubble-registered.com","amount": "14.50","product_id": 1,"product_type": "domain-registration","product_reference": "bubble-registered.com"}]}),
+            Charge({"invoiced_at": "2023-08-17T05:57:53Z","total_amount": "14.50","balance_amount": "0.00","reference": "2-2","state": "refunded","items":[{"description": "Register example.com","amount": "14.50","product_id": 2,"product_type": "domain-registration","product_reference": "example.com"}]}),
+            Charge({"invoiced_at": "2023-10-24T07:49:05Z","total_amount": "1099999.99","balance_amount": "0.00","reference": "4-2","state": "collected","items":[{"description": "Test Line Item 1","amount": "99999.99","product_id": None,"product_type": "manual","product_reference":None},{"description": "Test Line Item 2","amount": "1000000.00","product_id": None,"product_type": "manual","product_reference":None}]}),
+        ])
+
+    @responses.activate
+    def test_list_charges_fail_400_bad_filter(self):
+        responses.add(DNSimpleMockResponse(method=responses.GET,
+                                           path='/1010/billing/charges',
+                                           fixture_name='listCharges/fail-400-bad-filter'))
+        try:
+            self.billing.list_charges(1010)
+            assert False
+        except DNSimpleException as e:
+            self.assertEqual(e.message, "Invalid date format must be ISO8601 (YYYY-MM-DD)")
+
+    @responses.activate
+    def test_list_charges_fail_403(self):
+        responses.add(DNSimpleMockResponse(method=responses.GET,
+                                           path='/1010/billing/charges',
+                                           fixture_name='listCharges/fail-403'))
+        try:
+            self.billing.list_charges(1010)
+            assert False
+        except DNSimpleException as e:
+            self.assertEqual(e.message, "Permission Denied. Required Scope: billing:*:read")
